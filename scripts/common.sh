efe9b5a3b7cd929c24f8dfed8379a61bdaea4cbb
sanglv97 <76397656+sanglv97@users.noreply.github.com>
2024-01-09 02:15:29 +0700

    Update common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -4,9 +4,27 @@
 
 set -euxo pipefail
 
+# IPtables to see bridged traffic
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+# Apply sysctl params without reboot
+sudo sysctl --system
+
 # Variable Declaration
 
-KUBERNETES_VERSION="1.28.1-00"
+KUBERNETES_VERSION="1.28.2-00"
 
 # disable swap
 sudo swapoff -a
@@ -70,7 +88,7 @@ sudo apt-get install -y kubelet="$KUBERNETES_VERSION" kubectl="$KUBERNETES_VERSI
 sudo apt-get update -y
 sudo apt-get install -y jq
 
-local_ip="$(ip --json addr show eth0 | jq -r '.[0].addr_info[] | select(.family == "inet") | .local')"
+local_ip="$(ip --json addr show ens160 | jq -r '.[0].addr_info[] | select(.family == "inet") | .local')"
 cat > /etc/default/kubelet << EOF
 KUBELET_EXTRA_ARGS=--node-ip=$local_ip
 EOF
